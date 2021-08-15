819c1a422ea8606937e806a8711fff98b1f223be
heimu <heimu@helijia.com>
2021-08-15 10:17:55 +0800

    feat: 结构目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,61 +1,33 @@
 
 module.exports = {
-  title: '黑木的博客',
+  title: '黑木小课堂开课了',
   description: 'waiting for some one',
   base: '/Blog/',
   themeConfig: {
+    lastUpdated: 'Last Updated', // string | boolean
     nav: [
       { text: '主页', link: '/' },
-      { text: '知识',
+      { text: '前端',
         items: [
-          { text: 'JavaScript', link: '/knowledge/js/' },
-          { text: 'css', link: '/knowledge/css/' },
-          { text: '网络', link: '/knowledge/network/' },
-          { text: '数据结构', link: '/knowledge/dataStructure/' },
+          { text: 'JavaScript', link: '/web/js/' },
+          { text: 'css', link: '/web/css/' },
+          { text: '网络', link: '/web/network/' },
+          { text: '数据结构', link: '/web/dataStructure/' },
         ]
       },
-      { text: '工程', link: '/work/' },
-      { text: '杂文', link: '/essay/' },
+      { text: '工程', link: '/wheel/' ,
+        items: [
+          { text: 'webpack', link: '/wheel/webpack' }
+        ]
+      },
+      { text: '收藏', link: '/collect/' },
+      { text: 'Go', link: '/go/' },
+      { text: 'Node', link: '/node/' },
+      { text: '日记', link: '/diary/' },
       { text: '关于', link: '/about/' },
       { text: 'Github', link: 'https://github.com/Owen-MS/Blog' },
     ],
-    sidebar: {
-      '/knowledge/js/': [
-        {
-          title: 'javascript',
-          children: [
-            '',
-            '1.type',
-            '1.typeDetail',
-            '2.scope',
-            '3.this',
-            '4.oop',
-            '5.async',
-            'nojquery'
-          ]
-        }
-      ],
-      '/knowledge/learn/':[
-        {
-          title: '学习前端心得',
-          children: [
-            '',
-            'develop.md',
-            'more.md',
-            'ssh.md'
-          ]
-        }
-      ],
-      '/essay/':[
-        {
-          title: '收藏的杂文',
-          children: [
-            '',
-            '20190103',
-          ]
-        }
-      ]
-    },
+    sidebar: 'auto',
     sidebarDepth: 1,
   },
 }
